acb224775ddf096a4b57c53bf5bed3dddff7f112
begibeggineta <bergthorbi223@nemi.tskoli.is>
2017-01-25 16:23:47 +0000

    nú skrifar þetta línur

--- git_gso.py
+++ git_gso.py
@@ -6,3 +6,8 @@ F = open(Text+".txt","w+")
 
 F.write("Hello my honey Hello my baby hello my night time gaaal")
 F.close()
+F = open(Text+".txt","+w")
+innhald = input("Hvað viltu skrifa inn í texta skjalið")
+for i in range(0,3):
+    F.write(innhald +"\n")
+F.close()
